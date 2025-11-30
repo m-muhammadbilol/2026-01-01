c0a33411d784196ba8ef83085ad4cb2959db5701
Muhammadbilol <muhammadbilolmamasidiqov316@gmail.com>
2025-11-30 19:00:17 +0500

    gd

---
+++ app.js
@@ -0,0 +1,32 @@
+let day = prompt("Enter Todays Day: ")
+alert("You can show the result in CONSOLE: ")
+if (day == 'monday'){
+    console.log("Chest Day ");
+    console.log("Workout: 1.Pull-Ups , 2.with Doumbells , 3.Push-Ups , 4.Run 2km ! ");
+    console.log("Good luck! 💪");
+}else if(day == 'tuesday'){
+    console.log("Today is Restday bro 🫱🏻‍🫲🏻");
+    
+}else if (day == 'wednesday') {
+    console.log("Upperbody Day");
+    console.log('Wourkout: 1.Push-Ups-200, 2.With high kg-an 30min, 3.Abs-30min, 4.Shoulders-20min, 5.lats-20min ');
+    console.log("Good luck bro, You can do it   🫱🏻‍🫲🏻");   
+}else if(day == "thursday"){
+    console.log("Today is Restday bro 🫱🏻‍🫲🏻")
+}else if (day =="friday"){
+    console.log("First go MOSQUE")
+    console.log("and leg Day, friday's workout:1.sit ups-40min, 2.run 5km in outdoor!")
+}else if(day == "saturday"){
+   console.log("Today is Restday bro 🫱🏻‍🫲🏻");
+}else if (day == 'sunday'){
+    console.log("Today is sunday, eat healthy bro 🫱🏻‍🫲🏻");
+}else{
+    console.log('Enter');
+    console.log("monday – Dushanba");
+    console.log("tuesday – Seshanba");
+    console.log("wednesday – Chorshanba");
+    console.log("thursday – Payshanba");
+    console.log("friday – Juma");
+    console.log('saturday – Shanba');
+    console.log('sunday – Yakshanba');
+}
